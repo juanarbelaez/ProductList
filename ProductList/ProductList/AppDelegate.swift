64b04758415a430223e57c1eb05b860e7c0338ed
Juan Arbelaez <juan.arbelaez316@gmail.com>
2025-03-03 10:14:24 -0500

    Configuración proyecto
    Se configura el proyecto para ser desarrollado sin Storyboards

---
+++ ProductList/ProductList/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ProductList
+//
+//  Created by Juan Arbelaez on 3/03/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+}
+
---
+++ ProductList/ProductList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductList
+//
+//  Created by Juan Arbelaez on 3/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
